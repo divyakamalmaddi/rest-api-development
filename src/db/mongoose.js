84ea0c1ba64caa1247ff812811a8ff9b652143cf
Kowshik Sundararajan <kowshik.sundararajan@gmail.com>
2018-02-18 17:14:50 +0800

    Hash user's password before storing it in the db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,9 +4,9 @@ const assert = require('assert');
 // Making mongoose use the default promise and not a third-party promise
 mongoose.Promise = global.Promise;
 
-const url = 'mongodb://localhost:27017/test';
+const url = 'mongodb://localhost:27017/cs5331';
 
 mongoose.connect(url, (error, db) => {
   assert.equal(null, error);
-  console.log('Connected correctly to server', db);
+  console.log('Connected correctly to server');
 });
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Schema = { mongoose };
 
@@ -15,6 +16,22 @@ const UserSchema = new Schema({
     type: Number,
     required: true,
   },
+});
+
+UserSchema.pre('save', (next) => {
+  const user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (error, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) console.error(error);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model('User', UserSchema);
